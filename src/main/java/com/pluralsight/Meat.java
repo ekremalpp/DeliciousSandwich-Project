61a1f3acd5f4e54378dfdf09a17e0f942ebba017
Ekrem <ealp@pgh.yearup.org>
2024-11-12 16:01:52 -0500

    calculatePrice method overridden in Meat class

--- src/main/java/com/pluralsight/Meat.java
+++ src/main/java/com/pluralsight/Meat.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
 public class Meat extends Topping{
+    //Costructor
+    public Meat(String name) {
+        super(name);
+    }
+    @Override
+    public double calculateTotalPrice(int size) {
+        double price = 0.0;
+
+        switch (size) {
+            case 4:
+                price = 1.00;  // 4" için fiyat
+                break;
+            case 8:
+                price = 2.00;  // 8" için fiyat
+                break;
+            case 12:
+                price = 3.00;  // 12" için fiyat
+                break;
+        }
+        return price;
+    }
 }
