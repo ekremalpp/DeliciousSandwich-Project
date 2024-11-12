e66b3c1185db83bc5102e05381c8618e0aebf0b6
Ekrem <ealp@pgh.yearup.org>
2024-11-12 15:26:29 -0500

    added variable , canstuctor and getter

--- src/main/java/com/pluralsight/Topping.java
+++ src/main/java/com/pluralsight/Topping.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public  abstract class Topping {
+    private String name ;
+
+    public Topping(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
