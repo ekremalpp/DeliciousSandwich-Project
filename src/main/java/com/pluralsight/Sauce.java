53c090fe67efa58229a6640d04a62629d656fbf6
Ekrem <ealp@pgh.yearup.org>
2024-11-12 17:03:14 -0500

    calculatePrice method overridden in souse class

--- src/main/java/com/pluralsight/Sauce.java
+++ src/main/java/com/pluralsight/Sauce.java
@@ -4,4 +4,9 @@ public class Sauce extends Topping {
     public Sauce(String name) {
         super(name);
     }
+
+    @Override
+    public double calculateTotalPrice(int size) {
+        return 0.0; //sauce prices are fixed
+    }
 }
