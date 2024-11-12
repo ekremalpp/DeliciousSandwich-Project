aeef08d4bd9e8290f15a7ce789449cc338801a1d
Ekrem <ealp@pgh.yearup.org>
2024-11-12 17:32:10 -0500

    Added variables for bread type, size, toppings, and toasted state

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -1,5 +1,12 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sandwich implements IPriceable{
+    private String breadType;
+    private int size;
+    private List<Topping> toppings = new ArrayList<>();
+    private boolean toasted;
 
 }
