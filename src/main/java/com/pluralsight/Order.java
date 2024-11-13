5385a3863b8a8c7fe08b523707ca482bb27bee15
Ekrem <ealp@pgh.yearup.org>
2024-11-13 07:33:06 -0500

    Initialize lists for sandwiches, drinks, and chips in the Order class

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<Sandwich> sandwiches = new ArrayList<>();
+    private List<Drink> drinks = new ArrayList<>();
+    private List<Chips> chips = new ArrayList<>();
 }
