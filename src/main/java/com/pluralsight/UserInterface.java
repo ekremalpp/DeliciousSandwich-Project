641657ced6c9ec6d680e673f5ab35aa26e42ef68
Ekrem <ealp@pgh.yearup.org>
2024-11-13 10:59:32 -0500

    Create new order and show order menu

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -14,7 +14,7 @@ public class UserInterface {
             System.out.println("1) New Order");
             System.out.println("2) Exit");
 
-            int selection = getUserSelection();  //
+            int selection = getUserSelection();  //We get selection from the user
 
             switch (selection) {
                 case 1:
@@ -27,7 +27,26 @@ public class UserInterface {
                     System.out.println("Invalid selection. Please try again.");
             }
         }
+    }
+
+    public void createNewOrder() {
+
+        System.out.println("\nCreating a new order...");
+
+        // //Entering the order menu
+        while (true) {
+            System.out.println("\nOrder Menu:");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) Checkout");
+            System.out.println("0) Cancel Order");
+
+            int selection = getUserSelection();
+
+
         }
     }
+}
 
 
