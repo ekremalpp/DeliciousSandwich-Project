028cca0e3b8acb8c728aa5f7aeb6f322e8618eab
Ekrem <ealp@pgh.yearup.org>
2024-11-13 07:35:10 -0500

    added addSandwich method in order class

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -7,4 +7,8 @@ public class Order {
     private List<Sandwich> sandwiches = new ArrayList<>();
     private List<Drink> drinks = new ArrayList<>();
     private List<Chips> chips = new ArrayList<>();
+
+    public void addSandwich(Sandwich sandwich) {
+        sandwiches.add(sandwich);
+    }
 }
