cd439be04fc2d67e1a846fe7eeefe5ec607c45e0
Ekrem <ealp@pgh.yearup.org>
2024-11-13 10:48:41 -0500

    added displayMainMenu method in userInterface class

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -7,4 +7,27 @@ public class UserInterface {
     Scanner scanner = new Scanner(System.in);
     Order currentOrder = new Order(); // The object that will hold the current order
 
-}
+    //Main Menu: Receives main menu selection from the user
+    public void displayMainMenu() {
+        while (true) {
+            System.out.println("\nWelcome to DeliciousSandwiches!");
+            System.out.println("1) New Order");
+            System.out.println("2) Exit");
+
+            int selection = getUserSelection();  //
+
+            switch (selection) {
+                case 1:
+                    createNewOrder();  //
+                    break;
+                case 0:
+                    System.out.println("Exiting the application...");
+                    return;  //
+                default:
+                    System.out.println("Invalid selection. Please try again.");
+            }
+        }
+        }
+    }
+
+
