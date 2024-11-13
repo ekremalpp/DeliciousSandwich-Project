e38735b17c1657ebb911a152500bd2132a5e7c32
Ekrem <ealp@pgh.yearup.org>
2024-11-13 10:41:51 -0500

    Initialize scanner and currentOrder

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,10 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
+
+    Scanner scanner = new Scanner(System.in);
+    Order currentOrder = new Order(); // The object that will hold the current order
 
 }
