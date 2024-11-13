1ce83381e11e8e9d69099c0af0ec882a8724219b
Ekrem <ealp@pgh.yearup.org>
2024-11-13 09:46:22 -0500

    Write order details to a receipt file with error handling

--- src/main/java/com/pluralsight/OrderFileManager.java
+++ src/main/java/com/pluralsight/OrderFileManager.java
@@ -1,6 +1,11 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class OrderFileManager {
 
@@ -10,12 +15,24 @@ public class OrderFileManager {
 
         //We determine the file path
         File receptsFolder = new File("receipts");
-        
+
         if(!receptsFolder.exists()) {
             receptsFolder.mkdirs(); // It will create receipts folder
 
             //Create the file object
             File receiptFile = new File(receptsFolder, fileName);
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(receiptFile))) {
+                //write order's information
+
+                writer.write("Thank you for your order!\n\n");
+                writer.write("Order Details:\n");
+                writer.write(order.toString());  // Write details of order
+                writer.write("\nTotal Price: $" + order.calculateTotalPrice() + "\n");
+            } catch (IOException e) {
+                System.out.println("An error occurred while saving the receipt.");
+                e.printStackTrace();
+            }
         }
     }
 }
