bfff5e4d1c0497344839d6cacfe6af079b2ebf41
Ekrem <ealp@pgh.yearup.org>
2024-11-12 19:31:00 -0500

    added constructor in Drink class

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -3,4 +3,9 @@ package com.pluralsight;
 public class Drink implements IPriceable{
     private String size;
     private String flavor;
+
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
 }
