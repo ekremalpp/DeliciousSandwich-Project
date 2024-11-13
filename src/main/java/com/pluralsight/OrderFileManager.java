9e97f6f2a783cf7a0286b0184bd9cd4c6ed445ba
Ekrem <ealp@pgh.yearup.org>
2024-11-13 09:34:40 -0500

    created receipts folder and prepare the file for saving the receipt

--- src/main/java/com/pluralsight/OrderFileManager.java
+++ src/main/java/com/pluralsight/OrderFileManager.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
+import java.io.File;
+
 public class OrderFileManager {
+
+    public void saveReceipt(Order order) {
+        //Creating File Name
+        String fileName = getReceiptFileName();
+
+        //We determine the file path
+        File receptsFolder = new File("receipts");
+        
+        if(!receptsFolder.exists()) {
+            receptsFolder.mkdirs(); // It will create receipts folder
+
+            //Create the file object
+            File receiptFile = new File(receptsFolder, fileName);
+        }
+    }
 }
